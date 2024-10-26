96021066c20987f4feaca3edb0e3929c4a86adb5
Max Erastov <max.erastov@gmail.com>
2024-10-26 17:48:20 +0300

    FlatList

--- App.js
+++ App.js
@@ -1,20 +1,114 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  View,
+  ScrollView,
+  FlatList,
+  Text,
+  Image,
+  StyleSheet,
+  RefreshControl,
+} from 'react-native';
+import styled from 'styled-components/native';
+import React, {useState, useCallback} from 'react';
+
+const data = [
+  {
+    id: 1,
+    header: 'Javascript',
+    text: 'Almost 3 years of learning on the Hexlet platform',
+    logo: require('./assets/js_arcade_64.png'),
+  },
+  {
+    id: 2,
+    header: 'Ruby',
+    text: 'A few learning projects for fun',
+    logo: require('./assets/ruby_arcade_64.png'),
+  },
+  {
+    id: 3,
+    header: 'Python',
+    text: 'The basics and a little deeper to try other popular languages',
+    logo: require('./assets/python_arcade_64.png'),
+  },
+];
+
+const Item = ({item}) => (
+  <ItemContainer>
+    <LanguageContainer>
+      <Image source={item.logo} style={styles.logo}/>
+      <Text style={styles.header}>{item.header}</Text>
+    </LanguageContainer>
+    <DetailsContainer>
+      <Text style={styles.text}>{item.text}</Text>
+    </DetailsContainer>
+  </ItemContainer>
+);
 
 export default function App() {
+  const [refreshing, setRefreshing] = useState(false);
+  const onRefresh = useCallback(() => {
+    setRefreshing(true);
+    setTimeout(() => {
+      setRefreshing(false);
+    }, 2000);
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ScrollView
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh}/>
+        }>
+        <FlatList
+          data={data}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => (<Item item={item}></Item>)}
+        />
+      </ScrollView>
     </View>
   );
-}
-
+};
+  
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: 'white',
+    padding: 10,
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingTop: 25,
+  },
+  header: {
+    fontSize: 24,
+    fontWeight:'bold',
+    textAlign: 'center',
+  },
+  text: {
+    fontSize: 20,
+    // fontStyle: 'italic',
+    textAlign: 'center',
+  },
+  logo: {
+    width: 64,
+    height: 64,
   },
 });
+
+const ItemContainer = styled.View`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  border: 2px;
+  padding: 10px;
+  margin: 0px 10px 10px 10px;
+  border-radius: 10px;
+  font-family: sans-serif;
+`
+const LanguageContainer = styled.View`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`
+const DetailsContainer = styled.View`
+  display: flex;
+  flex-direction: row;
+`
---
+++ README.md
@@ -0,0 +1,1 @@
+[<video src="./demo.mp4" width="320" height="240" controls></video>](https://github.com/user-attachments/assets/a3d6bc11-31e7-454c-9ed2-360e100b3ba6)
